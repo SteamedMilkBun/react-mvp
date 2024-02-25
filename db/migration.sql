5569506997c8222d0e1b716bc5dcfc0a499bdece
Lance Tse <steam.bao666@gmail.com>
2024-02-25 15:42:11 -0800

    server js has all 5 routes

--- db/migration.sql
+++ db/migration.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS dataCard;
+DROP TABLE IF EXISTS datacards;
 
-CREATE TABLE dataCard (
+CREATE TABLE datacards (
     dc_id SERIAL PRIMARY KEY,
     dc_title VARCHAR(50),
     dc_desc VARCHAR(255)
--- server.js
+++ server.js
@@ -4,15 +4,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
+    connectionString: process.env.LOCAL_DB_URL
 })
 
 pool.connect()
     .then((client) => {
-        console.log(`Connected to postgres using connection string ${process.env.DATABASE_URL}`);
+        console.log(`Connected to postgres using connection string ${process.env.LOCAL_DB_URL}`);
         client.release();
     })
     .catch((err)=>{
@@ -21,4 +21,79 @@ pool.connect()
 
     const app = express();
     app.use(express.json());
-    app.use(express.static("public"));
+    app.use(express.static("dist"));
+
+    app.get('/datacards', (req, res) => {
+        pool.query('SELECT * FROM datacards')
+        .then((data) => {
+            console.log(data.rows);
+            res.json(data.rows);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        })
+    })
+
+    app.get('/datacards/:id', (req, res) => {
+        const id = Number.parseInt(req.params.id);
+        pool.query('SELECT * FROM datacards WHERE dc_id = $1', [id])
+        .then((data) => {
+            console.log(data.rows[0]);
+            res.json(data.rows[0]);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        })
+    })
+
+    app.post('/datacards', (req, res) => {
+        const dc_title = req.body.dc_title;
+        const dc_desc = req.body.dc_desc;
+
+        pool.query(`INSERT INTO datacards (dc_title, dc_desc) VALUES ($1, $2) RETURNING *`, [dc_title, dc_desc])
+        .then((data) => {
+            console.log("Added: ", dc_title, "and", dc_desc);
+            console.log(data.rows[0]);
+            res.json(data.rows[0]);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        })
+    })
+
+    app.patch('/datacards/:id', (req, res) => {
+        const id = Number.parseInt(req.params.id);
+        const dc_title = req.body.dc_title;
+        const dc_desc = req.body.dc_desc;
+
+        pool.query(`UPDATE datacards SET 
+                    dc_title = COALESCE($1, dc_title),
+                    dc_desc = COALESCE($2, dc_desc) WHERE
+                    dc_id = $3 RETURNING *`, [dc_title, dc_desc, id])       
+        .then((data) => {
+            console.log(data.rows[0]);
+            res.json(data.rows[0]);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        })
+    })
+
+    app.delete('/datacards/:id', (req, res) => {
+        const id = req.params.id;
+        
+        pool.query(`DELETE FROM datacards WHERE dc_id = $1`, [id])
+        .then((data) => {
+            res.json(data.rows[0]);
+        })
+        .catch((err) => {
+            console.error(err);
+            res.sendStatus(500);
+        })
+    })
+
+    app.listen(PORT, () => console.log("Listening to port: ", PORT));
