665e0e49cf84d560446bb265a818bb80162122d5
Lance Tse <steam.bao666@gmail.com>
2024-02-27 14:20:02 -0800

    edge case for empty title post

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,32 +12,38 @@ function Navbar ({currentDC, setCurrentDC}) {
         const inputTitle = document.getElementById('input-title').value;
         const inputDesc = document.getElementById('input-desc').value;
         const url = 'https://react-mvp-ec68.onrender.com/datacards';
-        const dataToSend = {
-            dc_title: inputTitle,
-            dc_desc: inputDesc
-        };
-        console.log(dataToSend);
 
-        const options = {//define options for POST request
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }, //set content type to JSON
-            body: JSON.stringify(dataToSend) // Convert the data object to JSON string,
-        };
-        //make POST request via fetch to server
-        fetch(url, options)
-        .then((response) => {
-            return response.json();//convert json response into js object we can use
-        })
-        .then((data) => {//take the resulting js data (object) and
-            //handle that js data
-            console.log("successfully posted: ", data);
-            setCurrentDC({});//set currentDC back to empty to rerender display all with posted data
-        })
-        .catch((err) => {//catch fetch errors
-            console.error(err);
-        })
+        if (inputTitle === '') {
+            alert('Title is empty')
+            return;
+        } else {
+            const dataToSend = {
+                dc_title: inputTitle,
+                dc_desc: inputDesc
+            };
+            console.log(dataToSend);
+    
+            const options = {//define options for POST request
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                }, //set content type to JSON
+                body: JSON.stringify(dataToSend) // Convert the data object to JSON string,
+            };
+            //make POST request via fetch to server
+            fetch(url, options)
+            .then((response) => {
+                return response.json();//convert json response into js object we can use
+            })
+            .then((data) => {//take the resulting js data (object) and
+                //handle that js data
+                console.log("successfully posted: ", data);
+                setCurrentDC({});//set currentDC back to empty to rerender display all with posted data
+            })
+            .catch((err) => {//catch fetch errors
+                console.error(err);
+            })
+        }
     }
 
     return (
